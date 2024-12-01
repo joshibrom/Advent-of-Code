583251cca789306a59e9a7cf936765bac4be749e
Joshua Ibrom <joshuaibrom@gmail.com>
2024-12-01 13:31:01 -0600

    Add docstrings to C helper functions.

--- 2024/c/helpers.h
+++ 2024/c/helpers.h
@@ -3,12 +3,33 @@
 #include <stdio.h>
 #include <string.h>
 
+/** 
+ * Ensures that a given invocation of an AoC solution has 2 arguments:
+ * 1. The program name.
+ * 2. The input file path.
+ * 
+ * Will `panic` if these conditions are not met.
+ */
 void ensureCommandParameters(int argc);
 
-void expect(void* ptr, const char* msg);
+/** 
+ * Verifies that a given `ptr` is not `NULL`. `panic`s if some null pointer is
+ * given with the supplied `msg` being printed to the terminal.
+*/
+void expect(const void* ptr, const char* msg);
+/**
+ * Prints the supplied `msg` to the terminal and exits with `EXIT_FAILURE`.
+ */
 void panic(const char* msg);
 
+/**
+ * Reads some input file `filename` to the pre-allocated `buf`.
+ */
 void readFile(const char* filename, char* buf);
+/**
+ * Parses lines from some `input` buffer to lines in `lineBuf` being
+ * pre-allocated to have at most capacity for `nLines` lines.
+ */
 void parseLines(const char* input, char** lineBuf, int nLines);
 
 
@@ -19,7 +40,7 @@ void ensureCommandParameters(int argc) {
     if (argc != 2) panic("Incorrect number of command arguments supplied.");
 }
 
-void expect(void* ptr, const char* msg) {
+void expect(const void* ptr, const char* msg) {
     if (ptr == NULL) panic(msg);
 }
 

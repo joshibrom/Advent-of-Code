4ef9d9165452cacd0fdf47ca71ff5234c1d89e58
Joshua Ibrom <joshuaibrom@gmail.com>
2025-12-01 21:57:50 -0600

    Cleanup D01 in Rust.
    I've cleaned my solution to show what I'd actually normally do.

--- 2025/rust/src/d01.rs
+++ 2025/rust/src/d01.rs
@@ -1,7 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     Left(usize),
     Right(usize),
+}
+
+impl Direction {
+    fn apply(self: Self, click: usize) -> usize {
+        let left = |n: usize, click: usize| match n > click {
+            true => 100 - (n - click),
+            false => click - n,
+        };
+        let right = |n: usize, click: usize| (click + n) % 100;
+        match self {
+            Self::Left(n) => left(n % 100, click),
+            Self::Right(n) => right(n % 100, click),
+        }
+    }
 }
 
 impl std::str::FromStr for Direction {
@@ -25,66 +39,32 @@ impl std::str::FromStr for Direction {
     }
 }
 
+fn perform_rotations(rots: Vec<Direction>) -> usize {
+    rots.into_iter()
+        .fold((50, 0), |(click, mut zero_count), d| {
+            let new_click = d.apply(click.clone());
+            zero_count += (new_click == 0) as usize;
+            (new_click, zero_count)
+        })
+        .1
+}
+
 fn parse_input(input: &str) -> Vec<Direction> {
     input.lines().map(|ln| ln.parse().unwrap()).collect()
 }
 
 fn process_p1(input: &str) -> usize {
-    let mut rb = std::collections::VecDeque::<usize>::with_capacity(100);
-    for i in 0..100 {
-        rb.push_back(i);
-    }
-    rb.rotate_right(50);
-    parse_input(input)
-        .into_iter()
-        .map(|d| {
-            let n1fn = |n: usize| n % 100;
-            let n2fn = |n: usize, n1: usize| (n - n1) % 100;
-            match d {
-                Direction::Left(n) => {
-                    let n1 = n1fn(n);
-                    let n2 = n2fn(n, n1);
-                    rb.rotate_left(n1);
-                    rb.rotate_left(n2);
-                }
-                Direction::Right(n) => {
-                    let n1 = n1fn(n);
-                    let n2 = n2fn(n, n1);
-                    rb.rotate_right(n1);
-                    rb.rotate_right(n2);
-                }
-            };
-            rb.front().unwrap().clone()
-        })
-        .filter(|n| n == &0)
-        .count()
+    perform_rotations(parse_input(input))
 }
 
 fn process_p2(input: &str) -> usize {
-    let mut rb = std::collections::VecDeque::<usize>::with_capacity(100);
-    for i in 0..100 {
-        rb.push_back(i);
-    }
-    rb.rotate_right(50);
-    parse_input(input)
-        .into_iter()
-        .flat_map(|d| match d {
-            Direction::Left(n) => (0..n)
-                .map(|_| {
-                    rb.rotate_left(1);
-                    rb.front().unwrap().clone()
-                })
-                .filter(|n| n == &0)
-                .collect::<Vec<usize>>(),
-            Direction::Right(n) => (0..n)
-                .map(|_| {
-                    rb.rotate_right(1);
-                    rb.front().unwrap().clone()
-                })
-                .filter(|n| n == &0)
-                .collect(),
-        })
-        .count()
+    let mut rots = Vec::new();
+    // Expand to single-click rotations
+    parse_input(input).into_iter().for_each(|d| match d {
+        Direction::Left(n) => (0..n).for_each(|_| rots.push(Direction::Left(1))),
+        Direction::Right(n) => (0..n).for_each(|_| rots.push(Direction::Right(1))),
+    });
+    perform_rotations(rots)
 }
 
 fn main() {
